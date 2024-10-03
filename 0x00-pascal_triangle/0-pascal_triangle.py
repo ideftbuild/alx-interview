f35d12a483c40815434bce463de9046d0aabfce3
ideftbuild <a.akanSWE@gamil.com>
2024-10-03 18:47:11 +0100

    Remove type hint and update module documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
 Module: 0-pascal_triangle
+
+Function: pascal_triangle(n)
 """
 
 
-def pascal_triangle(n: int) -> list[list[int]]:
+def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascal’s triangle of n
     """
